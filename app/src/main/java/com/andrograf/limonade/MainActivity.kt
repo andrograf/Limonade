314b04c46165fb6c3a20a690eebc983a12c8f6ed
Patricia P <andrograf31@gmail.com>
2023-07-27 17:19:32 +0200

    feat: create navbar

--- app/src/main/java/com/andrograf/limonade/MainActivity.kt
+++ app/src/main/java/com/andrograf/limonade/MainActivity.kt
@@ -1,17 +1,28 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.andrograf.limonade
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,8 +30,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,6 +48,23 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun NavBar(){
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "Lemonade",
+                fontWeight = FontWeight.Bold
+            )
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    )
+}
 
 @Composable
 fun InteractiveImageWithText(modifier: Modifier = Modifier){
@@ -57,25 +88,34 @@ fun InteractiveImageWithText(modifier: Modifier = Modifier){
         else -> R.string.restart
     }
 
-    Text(
-        text = "Lemonade",
-        //modifier = Modifier.background(Color.Yellow),
-        textAlign = TextAlign.Center
-    )
-    Button(
+    NavBar()
+
+    /*Button(
         onClick = { if(step < 4) step++ else step = 1 },
-        modifier =  modifier.width(imageResource.dp)) {
+        shape = RoundedCornerShape(15),
+        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer),
+        modifier =  modifier.wrapContentSize(Alignment.BottomStart).padding(10.dp)
+    ) {
         Column (
             horizontalAlignment = Alignment.CenterHorizontally,
-          //  verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.Center
         ){
 
-            Image(painter = painterResource(id = imageResource), contentDescription = imageDescription.toString())
+            Image(
+                painter = painterResource(id = imageResource),
+                contentDescription = imageDescription.toString(),
+                modifier = Modifier
+                    .padding(35.dp,15.dp)
+            )
             Spacer(modifier = modifier.height(16.dp))
-            Text(text = stringResource(textDescription))
+            Text(
+                text = stringResource(textDescription),
+                color = Color.Black,
+                style = MaterialTheme.typography.bodyLarge
+            )
 
         }
-    }
+    }*/
 
 
 }
@@ -93,7 +133,7 @@ fun LemonadeApp(modifier: Modifier = Modifier) {
 fun LemonadePreview() {
     LemonadeApp(
         modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
     )
 }
